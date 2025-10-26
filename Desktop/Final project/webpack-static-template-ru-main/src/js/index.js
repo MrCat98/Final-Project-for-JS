133d947ed827584ad468723e2eeb1ca515d76bdb
MrCat98 <maksampan4@gmail.com>
2025-10-26 20:57:27 +0900

    final-project

--- Desktop/Final project/webpack-static-template-ru-main/src/js/index.js
+++ Desktop/Final project/webpack-static-template-ru-main/src/js/index.js
@@ -1,3 +1,43 @@
 import '../scss/style.scss'
 
 console.log('It works!')
+let swiperInstance;
+
+function initSwiper() {
+  if (window.innerWidth < 768 && !swiperInstance) {
+    swiperInstance = new Swiper('.swiper', {
+      slidesPerView:'auto',
+      spaceBetween: 16,
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+  }
+
+  else if (window.innerWidth >= 768 && swiperInstance) {
+    swiperInstance.destroy(true, true);
+    swiperInstance = null;
+  }
+}
+
+initSwiper();
+
+window.addEventListener('resize', initSwiper);
+
+const box = document.getElementById('swiper-section')
+const changeButton = document.getElementById("watch")
+const buttonText = document.querySelector('.button-watch-text')
+
+changeButton.addEventListener('click', () =>{
+  box.classList.toggle('show-all');
+  changeButton.classList.toggle('active');
+
+  if(box.classList.contains('show-all')){
+    buttonText.textContent ='Скрыть';
+  } else {
+    buttonText.textContent = 'Показать все';
+  }
+
+})
