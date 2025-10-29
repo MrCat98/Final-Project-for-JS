a08563affdc4d68cd9f57a529988cc213286a6e4
MrCat98 <maksampan4@gmail.com>
2025-10-29 21:11:39 +0900

    add-swiper

--- Desktop/Final project/webpack-static-template-ru-main/src/js/index.js
+++ Desktop/Final project/webpack-static-template-ru-main/src/js/index.js
@@ -1,43 +1,59 @@
 import '../scss/style.scss'
+import'../scss/swiper-style.scss'
+
 
 console.log('It works!')
-let swiperInstance;
+let swiperFirst = null;
+let swiperSecond = null;
+let swiperThird = null;
 
-function initSwiper() {
-  if (window.innerWidth < 768 && !swiperInstance) {
-    swiperInstance = new Swiper('.swiper', {
+function initSwipers() {
+  if (window.innerWidth < 768 && !swiperFirst) {
+    swiperFirst = new Swiper('.swiper-first', {
       slidesPerView:'auto',
       spaceBetween: 16,
       loop: true,
       pagination: {
-        el: '.swiper-pagination',
+        el: '.swiper__pagination--first',
         clickable: true,
       },
     });
-  }
 
-  else if (window.innerWidth >= 768 && swiperInstance) {
-    swiperInstance.destroy(true, true);
-    swiperInstance = null;
+  } else if (window.innerWidth >= 768 && swiperFirst) {
+    swiperFirst.destroy(true, true);
+    swiperFirst = null;
+  }
+  if (window.innerWidth < 768 && !swiperSecond) {
+    swiperSecond = new Swiper('.swiper-second', {
+      slidesPerView: 'auto',
+      spaceBetween: 16,
+      loop: true,
+      pagination: {
+        el: '.swiper__pagination--second',
+        clickable: true,
+      },
+    });
+
+  } else if (window.innerWidth >= 768 && swiperSecond) {
+    swiperSecond.destroy(true, true);
+    swiperSecond = null;
+  }
+   if (window.innerWidth < 768 && !swiperThird) {
+    swiperSecond = new Swiper('.swiper-third', {
+      slidesPerView: 'auto',
+      spaceBetween: 16,
+      loop: true,
+      pagination: {
+        el: '.swiper__pagination--third',
+        clickable: true,
+      },
+    });
+  } else if (window.innerWidth >= 768 && swiperThird) {
+    swiperSecond.destroy(true, true);
+    swiperSecond = null;
   }
 }
 
-initSwiper();
+initSwipers();
 
-window.addEventListener('resize', initSwiper);
-
-const box = document.getElementById('swiper-section')
-const changeButton = document.getElementById("watch")
-const buttonText = document.querySelector('.button-watch-text')
-
-changeButton.addEventListener('click', () =>{
-  box.classList.toggle('show-all');
-  changeButton.classList.toggle('active');
-
-  if(box.classList.contains('show-all')){
-    buttonText.textContent ='Скрыть';
-  } else {
-    buttonText.textContent = 'Показать все';
-  }
-
-})
+window.addEventListener('resize', initSwipers);
